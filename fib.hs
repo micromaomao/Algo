8118f9287e89f909e5fb01e4baec12fc26fc62e3
Mao Wtm <micromaomao@gmail.com>
2017-01-21 12:44:06 +0800

    Optimize algorithm + mod

--- fib.hs
+++ fib.hs
@@ -1,3 +1,7 @@
+-- Fibonacci number
+-- Input: n
+-- Output: nth fibonacci number
+
 -- Naive algorithm, O(2^(n/2))
 fibNaive :: Integral i => Int -> i
 fibNaive n
@@ -6,16 +10,41 @@ fibNaive n
   | n == 1 = 1
   | otherwise = (fibNaive (n-1)) + (fibNaive (n-2))
 
-fibListConstruct :: Integral i => [i] -> [i]
-fibListConstruct [] = [0]
-fibListConstruct [0] = [1, 0]
-fibListConstruct (a:b:lst) = a+b:a:b:lst
-
-fibListContinue :: Integral i => Int -> [i] -> [i]
-fibListContinue l lst
-  | l <= 0 = []
-  | length lst >= l = lst
-  | otherwise = fibListContinue l (fibListConstruct lst) 
-
+-- Fast algorithm, O(n^2) -- n^2 because adding big numbers is O(n) not O(1)
 fibList :: Integral i => Int -> i
-fibList n = head (fibListContinue n [])
+fibList 0 = 0
+fibList n = fst ((iterate fibFromLastTwo (1, 0)) !! (n - 1))
+  where fibFromLastTwo (b, a) = (a + b, b)
+
+-- O(n)
+fibLastDigit :: Int -> Int
+fibLastDigit 0 = 0
+fibLastDigit n = fst (head $ drop (n - 1) (iterate fibLastDigitFromLastTwo (1, 0)))
+  where fibLastDigitFromLastTwo (b, a) = ((a + b) `mod` 10, b)
+
+-- Mostly constant time
+fibMod :: Int -> Int -> Int
+fibMod mo n
+  | mo <= 1 = 0
+  | otherwise = period !! (n `mod` (length period))
+    where period = fibModPeriod mo
+
+fibModFromLastTwo :: Int -> (Int, Int) -> (Int, Int)
+fibModFromLastTwo mo (b, a) = ((a + b) `mod` mo, b)
+
+fibModInf :: Int -> [Int]
+fibModInf mo | mo <= 1 = repeat 0
+fibModInf mo = 0 : moLst
+  where moLst = map fst $ iterate (fibModFromLastTwo mo) (1, 0)
+
+fibModPeriod :: Int -> [Int]
+fibModPeriod mo = takePeriod 3 infLst
+  where infLst = fibModInf mo
+        takePeriod index lst | take 3 lst /= [0, 1, 1] = error "Invalid list"
+        takePeriod index lst = if ((take 3 (drop index lst)) == [0, 1, 1]) then (take index lst) else (takePeriod (index+1) lst)
+
+fibSumMod :: Int -> Int -> Int
+fibSumMod mo sumTo | sumTo <= 1 = 0
+fibSumMod mo sumTo = theSum $ iterate fibSumIterate (1, 1, 0) !! (sumTo - 1)
+  where fibSumIterate (sum, lastB, lastA) = (let nFib = (lastB + lastA) `mod` mo in ((sum + nFib) `mod` mo, nFib, lastB))
+        theSum (sum,_,_) = sum
--- gcd.hs
+++ gcd.hs
@@ -1,14 +1,23 @@
-gcdNaive :: (Integral n) => n -> n -> n
-gcdNaive a b
-  | a == 0 = b
-  | b == 0 = a
-  | a < 0 || b < 0 = error "a < 0 || b < 0"
-  | otherwise = last (filter (\d -> d /= 0) [if snd (divMod a d) == 0 && snd (divMod b d) == 0 then d else 0 | d <- [1..(a+b)]])
+-- Greatest common divider
 
-gcdRem :: (Integral n) => n -> n -> n
-gcdRem a b
+-- O(a+b)
+gcdNaive :: (Integral n) => (n, n) -> n
+gcdNaive (a, b)
   | a == 0 = b
   | b == 0 = a
   | a < 0 || b < 0 = error "a < 0 || b < 0"
-  | otherwise = gcdRem b aMod
-    where aMod = snd (divMod a b)
+  | otherwise = last (filter (\d -> d /= 0) [if mod a d == 0 && mod b d == 0 then d else 0 | d <- [1..(a+b)]])
+
+-- O(log(n))
+gcdRem :: (Integral n) => (n, n) -> n
+gcdRem (a, b)
+  | a == 0 = b
+  | b == 0 = a
+  | a < 0 || b < 0 = error "a < 0 || b < 0"
+  | otherwise = gcdRem (b, (mod a b))
+
+lcm :: (Integral i) => (i, i) -> i
+lcm (a, b)
+  | a == 0 || b == 0 = 0
+  | a == 1 || b == 1 = 1
+  | otherwise = a * b `div` (gcd a b)
